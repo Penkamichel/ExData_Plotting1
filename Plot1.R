b842fb68d08516624a3b5844756e1ae7fed0ff23
Tsubasa <tsubasa-ippon@hotmail.co.jp>
2017-03-04 05:07:18 +0900

    Plot1
    Plot1 script and graph

---
+++ Plot1.R
@@ -0,0 +1,12 @@
+#Load the data
+      fileURL <- "../household_power_consumption.txt"
+      fulldata <- read.table(fileURL, sep=";", header=TRUE, stringsAsFactors=FALSE, na.strings="?")
+      fulldata$Date <- as.Date(fulldata$Date, format="%d/%m/%Y")
+      subsetdata <- subset(fulldata, fulldata$Date=="2007-02-01" | fulldata$Date=="2007-02-02")
+      
+# Making plot
+      hist(subsetdata$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (killowatts)")
+
+# create png file
+      dev.copy(png, file="Plot1.png")
+      dev.off()  
